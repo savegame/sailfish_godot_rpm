40b8bf34204e8dcfce2adbf585f2c78c8183bf94
sashikknox <sashikknox@gmail.com>
2019-04-02 15:39:25 +0300

    + finish! now you can make RPM files

--- make_rpm.sh
+++ make_rpm.sh
@@ -3,52 +3,68 @@
 _pwd=`dirname $(readlink -e "$0")`
 
 _app_name="godot_game"
-_app_icon="godot_game_template.png"
+_app_long_name=""
+_app_icon=""
 _mer_sdk_ip="localhost"
 _mer_sdk_port="2222"
 _mer_target=""
 _pck_file=""
 _godot_binary=""
+_version="1.0.0"
 
 IFS='' read -r -d '' spec_file_data <<"EOF"
-#Hello world
-Name:    $appname$_$version$
-Summary: $appname$ package
-Version: 2.65
-Release: 1%{?dist}
-
-License: NOT
-Source0: $appname$.tar.xz
-AutoReq: yes
-
-BuildArch: x86_64
-BuildRoot: %{_topdir}/
+Name:       $application_name$
+Summary:    $application_long_name$
+Version:    $version$
+Release:    1
+Group:      Godot
+License:    LICENSE
+URL:        http://example.org/
+#Source0:    %{name}.tar.xz
+Requires:   SDL2
+Requires:   freetype
+Requires:   libpng
+Requires:   openssl
+Requires:   zlib
+Requires:   libvpx
+Requires:   libwebp
+Requires:   glib2
+Requires:   libaudioresource
 
 %description
-$appname$. Based on Godot Engine
+This game made with Godot Game Engine! Godot Engine version 3.1
 
 %prep
-mkdir -p $RPM_BUILD_ROOT/
-cd $RPM_BUILD_ROOT
-tar xJf  %{_topdir}/SOURCES/$libname$.tar.xz
+echo "Nothing to do here. Skip this step"
 
-%build 
-cd $RPM_BUILD_ROOT
+%build
+cd %{_topdir}/BUILD
+echo "Nothing to do here. Skip this step"
 
-
-%install 
- 
+%install
+rm -rf %{buildroot}
+mkdir -p %{buildroot}
+cp -r %{_topdir}/BUILD/usr %{buildroot}/
 
 %files
-%defattr(-,root,root)
-
-
-%clean 
-rm -fr %RPM_BUILD_ROOT
+%defattr(-,root,root,-)
+%{_bindir}
+%{_datadir}/%{name}
+%{_datadir}/applications/%{name}.desktop
 
 %changelog 
-* Tue Feb 12 2019 $app_name$
-- создан spec для $app_name$
+* $date$ Godot Game Engine
+- application $application_name$ packed to RPM
+EOF
+
+IFS='' read -r -d '' desktop_file_data <<"EOF"
+[Desktop Entry]
+Type=Application
+X-Nemo-Application-Type=sdl2
+Icon=/usr/share/$application_name$/$application_name$.png
+Exec=/usr/bin/$application_name$ --main-pack /usr/share/$application_name$/$application_name$.pck
+Name=$application_long_name$
+Name[en]=$application_long_name$
 EOF
 
 function parse_args() 
@@ -60,7 +76,7 @@ function parse_args()
         case "$1" in
         "-t" | "--target" )
             shift
-            _mer_target=$1
+            _mer_target="$1"
         ;;
         "-pck" | "--pck" )
             shift
@@ -70,8 +86,20 @@ function parse_args()
             shift 
             _godot_binary="$1"
         ;;
+        "-icon" | "--icon" )
+            shift
+            _app_icon="$1"
+        ;;
+        "-n" | "--name" )
+            shift 
+            _app_name="$1"
+        ;;
+        "-ln" | "--long-name" )
+            shift 
+            _app_long_name="$1"
+        ;;
         * )
-            echo "Unknown parameter $argc: "$1
+            echo "Unknown parameter $argc: $1"
         ;;
 		esac
         shift
@@ -84,6 +112,21 @@ function check_args()
     if [ -z $_pck_file ] ; then 
         echo "You should set path to pck file by --pck <path>"
         return 1
+    fi
+
+    if [ -z $_godot_binary ] ; then 
+        echo "You should set path to godot export template binary by -g/--godot <path>"
+        return 1
+    fi
+
+    if [ -z $_app_icon ] ; then 
+        echo "You should set path to game icon file by --icon <path> (PNG file)"
+        return 1
+    fi
+
+    if [ -z $_app_long_name ] ; then
+        echo "You dont set Appliction Human Readable Name, set app_name"
+        _app_long_name="${_app_name}"
     fi
 
     return 0
@@ -100,12 +143,10 @@ function check_mer_target()
             eval option[${target_count}]=$target
             ((target_count++))
         done
-        eval option[${target_count}]=\"All targets\"
-        ((target_count++))
+        # dont use all targets, bacuse we have only for one target binary
+        # eval option[${target_count}]=\"All targets\"
+        # ((target_count++))
         select opt in "${option[@]}" ; do
-            # case $opt in 
-            #     *) echo "invalid option: $opt" ;;
-            # esac 
             if [ -z "$opt" ] ; then
                 echo "Invalid option $REPLY"
             elif [[ $opt == "All targets" ]] ; then
@@ -125,24 +166,52 @@ function prepare_build_folder()
 {
     check_mer_target
     local current_target=""
+    local current_date="`LC_ALL=en_EN.UTF-8 date "+%a %b %d %Y"`"
+
     for current_target in ${_mer_target} ; do
         local current_build_root="${_pwd}/buildroot/$current_target"
         local sb2_command="sb2 -t $current_target "
+
         echo "Current target is \"$current_target\""
         # clear build folder 
-        if [ -f ${current_build_root} ] ; then
+        if [ -d ${current_build_root} ] ; then
             echo "Remove old build dir."
-            ${sb2_command} rm -fr ${current_build_root}
+            rm -fr ${current_build_root}
         fi
+
         # create RPM build folders 
         echo "Create directories {SOURCES,BUILD,SPECS} in ${current_build_root}."
-        ${sb2_command} mkdir -p "${current_build_root}"/{SOURCES,BUILD,SPECS}
-        # copy game resources
+        mkdir -p "${current_build_root}"/{SOURCES,BUILD,SPECS}
+        mkdir -p "${current_build_root}"/BUILD/usr/{bin,share}
+        mkdir -p "${current_build_root}"/BUILD/usr/share/{$_app_name,applications}        
+        # need make normal icons folder like /usr/share/icons/86x86/my_app_game.png
+        #${sb2_command} mkdir -p "${current_build_root}"/BUILD/usr/share/icons/{86x86,108x108,128x128,256x256}
 
+        # copy game resources
+        echo "Copy game resources to build rpm directory."
+        rsync -aP $_godot_binary "${current_build_root}"/BUILD/usr/bin/$_app_name
+        [ $? -ne 0 ] && return 1
+        rsync -aP $_pck_file "${current_build_root}"/BUILD/usr/share/$_app_name/$_app_name.pck
+        [ $? -ne 0 ] && return 1
+        rsync -aP $_app_icon "${current_build_root}"/BUILD/usr/share/$_app_name/$_app_name.png
+        [ $? -ne 0 ] && return 1
+        
+        echo "Generate ${_app_name}.desktop file."
+        echo "$desktop_file_data"|sed -e "s~\\\$application_name\\\$~$_app_name~g" -e "s~\\\$application_long_name\\\$~$_app_long_name~g">"${current_build_root}"/BUILD/usr/share/applications/${_app_name}.desktop
+        local spec_file_path="${current_build_root}/SPECS/${_app_name}.spec"
+
+        echo "Generate ${_app_name}.spec file."
+        echo "$spec_file_data"|sed -e "s~\\\$application_name\\\$~$_app_name~g" -e "s~\\\$application_long_name\\\$~$_app_long_name~g" -e "s~\\\$version\\\$~$_version~g" -e "s~\\\$date\\\$~${current_date}~g">"${spec_file_path}"
+
+        #echo "Create empty ${_app_name}.tar.gz archive"
+        #touch "${current_build_root}/SOURCES/${_app_name}.tar.gz"
+
+        echo "Pack all data to RPM file."
+        ${sb2_command} rpmbuild --define  "_topdir ${current_build_root}" -ba "${current_build_root}/SPECS/${_app_name}.spec" &>"${_pwd}/rpmbuild_${_app_name}.log"
     done
 }
 
-parse_args $@
+parse_args "$@"
 check_args
 [ $? -ne 0 ] && exit 1
 prepare_build_folder
